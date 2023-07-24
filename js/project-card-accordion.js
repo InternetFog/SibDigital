94ae469f0964199d1cf6ddf4164a3a512c654a8b
InternetFog <antoha_iv@list.ru>
2023-07-24 12:07:42 +0800

    Scritprs for accordion & Layout of similar project

---
+++ js/project-card-accordion.js
@@ -0,0 +1,14 @@
+const accordionOb = document.querySelectorAll(".project-card-accordion-block");
+if (accordionOb.length != 0) {
+	for (let i = 0; i < accordionOb.length; i++) {
+		let accordionItems = accordionOb[i].querySelectorAll('.project-card-accordion_list_item');
+		if (accordionItems.length != 0) {
+			for (let j = 0; j < accordionItems.length; j++) {
+				accordionItems[j].addEventListener('click', function() {
+					this.closest('.project-card-accordion-block').querySelector('.project-card-accordion_list_item.is-active').classList.remove('is-active');
+					this.classList.add('is-active');
+				});
+			}
+		}
+	}
+}
